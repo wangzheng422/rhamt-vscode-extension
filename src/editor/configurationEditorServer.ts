c43b159f2581a9c7c8139f4ac6bf381188f9d5c9
wzh <wangzheng422@gmail.com>
2020-12-30 18:07:27 +0800

    fix timeout issue

--- src/editor/configurationEditorServer.ts
+++ src/editor/configurationEditorServer.ts
@@ -51,7 +51,7 @@ export class ConfigurationEditorServer {
                         console.log(`Configuration server startup timeout failure. Notifying...`);
                         reject();
                     }
-                }, 25000000);
+                }, 2147483647);
             }).bind(this);
             this.server.on('listening', () => {
                 console.log(`Configuration server successfully started...`);
