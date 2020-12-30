6ece0ddb6002482236b92a2c1107678aaa867216
wzh <wangzheng422@gmail.com>
2020-12-30 16:54:22 +0800

    fix timeout for slow machine

--- src/editor/configurationEditorServer.ts
+++ src/editor/configurationEditorServer.ts
@@ -51,7 +51,7 @@ export class ConfigurationEditorServer {
                         console.log(`Configuration server startup timeout failure. Notifying...`);
                         reject();
                     }
-                }, 25000);
+                }, 2500000);
             }).bind(this);
             this.server.on('listening', () => {
                 console.log(`Configuration server successfully started...`);
